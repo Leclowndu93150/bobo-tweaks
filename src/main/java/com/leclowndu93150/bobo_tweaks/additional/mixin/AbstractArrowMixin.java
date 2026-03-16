2cd3cfa7b275cd67f1555480e6a34ab7693e1d47
Leclowndu93150 <xdr.a123454321@gmail.com>
2026-03-16 22:01:41 +0100

    i don't remember what this is

--- src/main/java/com/leclowndu93150/bobo_tweaks/additional/mixin/AbstractArrowMixin.java
+++ src/main/java/com/leclowndu93150/bobo_tweaks/additional/mixin/AbstractArrowMixin.java
@@ -1,58 +1,31 @@
 package com.leclowndu93150.bobo_tweaks.additional.mixin;
 
 import com.leclowndu93150.bobo_tweaks.effect.RejuvenatingShotsEffect;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.EntityHitResult;
-import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AbstractArrow.class)
 public abstract class AbstractArrowMixin {
 
-    @Inject(
+    @Redirect(
         method = "tick",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/entity/player/Player;canHarmPlayer(Lnet/minecraft/world/entity/player/Player;)Z"
-        ),
-        locals = LocalCapture.CAPTURE_FAILHARD,
-        cancellable = true
+        )
     )
-    private void onBeforeCanHarmPlayerCheck(CallbackInfo ci,
-            boolean flag,
-            Vec3 vec3,
-            BlockPos blockpos,
-            BlockState blockstate,
-            Vec3 vec32,
-            Vec3 vec33,
-            HitResult hitresult,
-            EntityHitResult entityhitresult,
-            Entity entity,
-            Entity entity1
-    ) {
+    private boolean onCanHarmPlayer(Player shooter, Player target) {
         AbstractArrow arrow = (AbstractArrow) (Object) this;
-
-        if (!(entity instanceof LivingEntity target)) {
-            return;
-        }
-
-        if (!(entity1 instanceof Player shooter)) {
-            return;
-        }
 
         if (RejuvenatingShotsEffect.tryHealAlly(arrow, shooter, target)) {
             arrow.discard();
-            ci.cancel();
+            return false;
         }
+
+        return shooter.canHarmPlayer(target);
     }
 }
